07b899f5563394fa17cb4e7f4106995ccfbca258
Dmitriy Panzhinskiy <pandimun@gmail.com>
2021-06-10 10:33:38 +0300

    create first components

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const Navbar = () => (
+  <div className='nav-left'>
+    <ul>
+      <li>Profile</li>
+      <li>Message</li>
+      <li>News</li>
+      <li>Music</li>
+    </ul>
+  </div>
+)
---
+++ src/components/ProfilePage.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export const ProfilePage = () => {
+  return (
+    <div className='content'>
+      <img src="https://helpx.adobe.com/content/dam/help/en/photoshop/using/convert-color-image-black-white/jcr_content/main-pars/before_and_after/image-before/Landscape-Color.jpg" alt="" />
+      
+      <div className="flex-row">
+        <div className="avatar">
+          <img src="https://www.pixsy.com/wp-content/uploads/2021/04/ben-sweet-2LowviVHZ-E-unsplash-1.jpeg" alt="" />
+        </div>
+        <div className="user-name"> User Name </div>
+      </div>
+    </div>
+  )
+}
