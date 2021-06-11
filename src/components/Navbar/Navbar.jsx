c2deb8271bb4a9ca018138f4f3208443fdd3f8e3
Dmitriy Panzhinskiy <pandimun@gmail.com>
2021-06-11 11:14:54 +0300

    edited other

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,11 +3,11 @@ import s from './Navbar.module.css'
 
 export const Navbar = () => (
   <div className={s.nav}>
-    <menu className={s.items}>
-      <a href='#2' className={s.item}>Profile</a>
-      <a href='#2' className={s.item}>Message</a>
-      <a href='#2' className={s.item}>News</a>
-      <a href='#2' className={s.item}>Music</a>
-    </menu>
+    <ul className={s.items}>
+      <li href='#2' className={s.item}>Profile</li>
+      <li href='#2' className={s.item}>Message</li>
+      <li href='#2' className={s.item}>News</li>
+      <li href='#2' className={s.item}>Music</li>
+    </ul>
   </div>
 )
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -5,12 +5,24 @@ export const Profile = () => {
   return (
     <div className={s.content}>
       <img src="https://helpx.adobe.com/content/dam/help/en/photoshop/using/convert-color-image-black-white/jcr_content/main-pars/before_and_after/image-before/Landscape-Color.jpg" alt="" />
-      
-      <div>
-        <div className="avatar">
-          <img src="https://www.pixsy.com/wp-content/uploads/2021/04/ben-sweet-2LowviVHZ-E-unsplash-1.jpeg" alt="" />
-        </div>
-        <div className="user-name"> User Name </div>
+      <div>New post 
+        <div> 
+          <textarea name="" id="" cols="90" rows="4"></textarea> 
+          <button>Create</button> 
+        </div> 
+      </div>
+
+      <div className={s.posts}>
+        <div>Post item</div>
+        <div>Post item</div>
+        <div>Post item</div>
+        <div>Post item</div>
+        <div>Post item</div>
+        <div>Post item</div>
+        <div>Post item</div>
+        <div>Post item</div>
+        <div>Post item</div>
+        <div>Post item</div>
       </div>
     </div>
   )
