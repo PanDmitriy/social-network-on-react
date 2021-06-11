6d49431f8d8e85d8a4da2a9ea48748006c5ac08f
Dmitriy Panzhinskiy <pandimun@gmail.com>
2021-06-11 14:57:51 +0300

    changes router

--- src/App.js
+++ src/App.js
@@ -4,7 +4,11 @@ import { Header } from './components/Header/Header';
 import { Navbar } from './components/Navbar/Navbar';
 import { Footer } from './components/Footer/Footer';
 import { UserCard } from './components/Profile/UserCard/UserCard';
-import { Dialogs} from './components/Dialogs/Dialogs'
+import { Dialogs} from './components/Dialogs/Dialogs';
+import { News } from './components/News/News';
+import { Music } from './components/Music/Music';
+import { Settings } from './components/Settings/Settings';
+
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
@@ -21,6 +25,15 @@ function App() {
               </Route>
               <Route path='/dialogs'>
                 <Dialogs/>
+              </Route>
+              <Route path='/news'>
+                <News/>
+              </Route>
+              <Route path='/music'>
+                <Music/>
+              </Route>
+              <Route path='/settings'>
+                <Settings/>
               </Route>
             </Switch>
           </div>
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import s from './Navbar.module.css';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 export const Navbar = () => (
   <div className={s.nav}>
     <nav className={s.items}>
-      <Link to='/profile' className={s.item}>Profile</Link>
-      <Link to='/dialogs' className={s.item}>Message</Link>
-      <Link to='/news' className={s.item}>News</Link>
-      <Link to='/music' className={s.item}>Music</Link>
-      <Link to='/settings' className={s.item}>Settings</Link>
+      <NavLink to='/profile' className={s.item}>Profile</NavLink>
+      <NavLink to='/dialogs' className={s.item}>Message</NavLink>
+      <NavLink to='/news' className={s.item}>News</NavLink>
+      <NavLink to='/music' className={s.item}>Music</NavLink>
+      <NavLink to='/settings' className={s.item}>Settings</NavLink>
     </nav>
   </div>
 )
