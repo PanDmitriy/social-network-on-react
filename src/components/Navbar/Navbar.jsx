1028e419dac3be390e798c13a8c2b373e6a300ba
Dmitriy Panzhinskiy <pandimun@gmail.com>
2021-06-11 15:40:22 +0300

    style active link

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -5,11 +5,11 @@ import { NavLink } from 'react-router-dom';
 export const Navbar = () => (
   <div className={s.nav}>
     <nav className={s.items}>
-      <NavLink to='/profile' className={s.item}>Profile</NavLink>
-      <NavLink to='/dialogs' className={s.item}>Message</NavLink>
-      <NavLink to='/news' className={s.item}>News</NavLink>
-      <NavLink to='/music' className={s.item}>Music</NavLink>
-      <NavLink to='/settings' className={s.item}>Settings</NavLink>
+      <NavLink to='/profile' className={s.item} activeClassName={s.activeLink}>Profile</NavLink>
+      <NavLink to='/dialogs' className={s.item} activeClassName={s.activeLink}>Message</NavLink>
+      <NavLink to='/news' className={s.item} activeClassName={s.activeLink}>News</NavLink>
+      <NavLink to='/music' className={s.item} activeClassName={s.activeLink}>Music</NavLink>
+      <NavLink to='/settings' className={s.item} activeClassName={s.activeLink}>Settings</NavLink>
     </nav>
   </div>
 )
