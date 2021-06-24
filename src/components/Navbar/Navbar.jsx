8eac3d0ad4657fefd9adbe3f81430bffda433c2e
Dmitriy Panzhinskiy <pandimun@gmail.com>
2021-06-24 20:45:15 +0300

    Navbar new link list

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,15 +1,51 @@
 import React from 'react';
 import s from './Navbar.module.css';
+import { Paper, MenuList, MenuItem, ListItemIcon,Typography } from '@material-ui/core';
+import DraftsIcon from '@material-ui/icons/Drafts';
+import AccountCircleIcon from '@material-ui/icons/AccountCircleOutlined';
+import PriorityHighIcon from '@material-ui/icons/PriorityHigh';
 import { NavLink } from 'react-router-dom';
 
 export const Navbar = () => (
+
   <div className={s.nav}>
-    <nav className={s.items}>
-      <NavLink to='/profile' className={s.item} activeClassName={s.activeLink}>Profile</NavLink>
-      <NavLink to='/dialogs' className={s.item} activeClassName={s.activeLink}>Message</NavLink>
-      <NavLink to='/news' className={s.item} activeClassName={s.activeLink}>News</NavLink>
-      <NavLink to='/music' className={s.item} activeClassName={s.activeLink}>Music</NavLink>
-      <NavLink to='/settings' className={s.item} activeClassName={s.activeLink}>Settings</NavLink>
-    </nav>
+    <Paper style={{width: '100%'}}>
+      <MenuList>
+      <NavLink to='/profile' className={s.item}>
+        <MenuItem>
+          <ListItemIcon>
+            <AccountCircleIcon/>
+          </ListItemIcon>
+          <Typography variant="inherit">Profile</Typography>
+        </MenuItem>
+        </NavLink>
+        <NavLink to='/dialogs' className={s.item}>
+        <MenuItem>
+          <ListItemIcon>
+            <PriorityHighIcon/>
+          </ListItemIcon>
+          <Typography variant="inherit">Message</Typography>
+        </MenuItem>
+        </NavLink>
+        <NavLink to='/news' className={s.item}>
+        <MenuItem>
+          <ListItemIcon>
+            <DraftsIcon/>
+          </ListItemIcon>
+          <Typography variant="inherit">News</Typography>
+        </MenuItem>
+        </NavLink>
+      </MenuList>
+    </Paper>
   </div>
+
+  // <div className={s.nav}>
+  //   <nav className={s.items}>
+  //     <NavLink to='/profile' className={s.item} activeClassName={s.activeLink}>Profile</NavLink>
+  //     <NavLink to='/dialogs' className={s.item} activeClassName={s.activeLink}>Message</NavLink>
+  //     <NavLink to='/news' className={s.item} activeClassName={s.activeLink}>News</NavLink>
+  //     <NavLink to='/music' className={s.item} activeClassName={s.activeLink}>Music</NavLink>
+  //     <NavLink to='/settings' className={s.item} activeClassName={s.activeLink}>Settings</NavLink>
+  //   </nav>
+  // </div>
 )
