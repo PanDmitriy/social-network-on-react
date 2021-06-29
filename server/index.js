8cfa59c4e3af1c0c40e915a4d55a266be4d9ddcc
Dmitriy Panzhinskiy <pandimun@gmail.com>
2021-06-29 09:47:29 +0300

    server. step 2

--- server/index.js
+++ server/index.js
@@ -1,10 +1,25 @@
 require('dotenv').config();
 const express = require('express');
-const sequelize = require('./db')
+const sequelize = require('./db');
+const models = require('./models/models');
+const cors = require('cors');
+const router = require('./routes/index');
+const errorHandler = require('./middleware/ErrorHandlingMiddleware')
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
 
-const app = express()
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use('/api', router);
+
+
+// Обработка ошибки, последний Middleware
+app.use(errorHandler);
+
+// app.get('/', (request, response) => {
+//   response.status(200).json({message: 'WORKING!!!'})
+// });
 
 const start = async () => {
   try {
