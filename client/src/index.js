0286f5768f77db3376506ea4e7f335439f201b8a
Dmitriy Panzhinskiy <pandimun@gmail.com>
2021-07-16 20:00:56 +0300

    clear file index.js

--- client/src/index.js
+++ client/src/index.js
@@ -3,27 +3,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 import store from './Redux/store';
 import { Provider } from 'react-redux';
 
-const rerenderEntireTree = (state) => {
-  console.log('Store: ', state);
-  ReactDOM.render(
-    <React.StrictMode>
-      <Provider store={store}>
-        <App />
-      </Provider>
-    </React.StrictMode>,
-    document.getElementById('root')
-  );
-}
+console.log('Store: ', store);
 
-rerenderEntireTree(store.getState());
-
-store.subscribe(() => {
-  const state = store.getState();
-  rerenderEntireTree(state);
-});
-
-reportWebVitals();
+ReactDOM.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
